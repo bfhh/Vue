b56cbb3dfe0120c96f0b7895ff069782098d560b
golanggo <golanggo@163.com>
2018-12-06 16:07:56 +0800

    city-search

---
+++ travel/src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名字或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height: .72rem
+    background: $bgColor
+    padding: 0 .1rem
+    .search-input
+      box-sizing: border-box
+      height: 0.62rem
+      width: 100%
+      line-height: 0.62rem
+      text-align: center
+      border-radius: .06rem
+      padding: 0 .2rem
+      color: #666
+</style>
