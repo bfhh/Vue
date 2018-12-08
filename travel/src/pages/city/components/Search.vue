528d6c500ba4d3cb2f4362b6b8b7a96d2e860e8f
golanggo <golanggo@163.com>
2018-12-08 16:21:58 +0800

    search finish

--- travel/src/pages/city/components/Search.vue
+++ travel/src/pages/city/components/Search.vue
@@ -1,12 +1,75 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名字或拼音">
+  <div>
+    <div class="search">
+      <input
+        class="search-input"
+        type="text"
+        placeholder="输入城市名字或拼音"
+        v-model="keyword"
+      >
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li
+          class="search-item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+        >
+          {{item.name}}
+        </li>
+        <li  class="search-item border-bottom" v-show="hasNoData">
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          // forEach后面跟的是函数
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 
@@ -25,4 +88,19 @@ export default {
       border-radius: .06rem
       padding: 0 .2rem
       color: #666
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    bottom: 0
+    left: 0
+    right: 0
+    background: #eee
+    .search-item
+      line-height: 0.62rem
+      padding-left: 0.2rem
+      background: #fff
+      color: #666
+
 </style>
